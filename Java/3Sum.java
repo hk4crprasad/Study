e898a85f1835b64dd51c86949b3420540f751889
hk4crprasad <hotahara12@gmail.com>
2023-11-09 21:40:27 +0530

    2

---
+++ Java/3Sum.java
@@ -0,0 +1,14 @@
+package com.piro;
+import java.util.*;
+class A {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter 1st num :- ");
+        int a = s.nextInt();
+        System.out.print("Enter 2nd num :- ");
+        int b = s.nextInt();
+        System.out.print("Enter 3rd num :- ");
+        int c = s.nextInt();
+        System.out.println("Sum = "+(a+b+c));
+        }
+}
